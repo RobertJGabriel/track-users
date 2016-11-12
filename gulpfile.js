b2af56b74d72ea0a444b3180f673adc6bb013a10
Robert James Gabriel <robert_gabriel@outlook.com>
2016-11-12 17:41:51 +0000

    Correct usage of strings

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,29 @@
 const gulp = require('gulp');
 const xo = require('gulp-xo');
 
-
 gulp.task('fix', () =>
     gulp.src('index.js')
     .pipe(xo({
-        "envs": [
-            "browser"
-        ],
-        "globals": [
-            "$",
-            "jQuery"
-        ],
-        "ignore": [
-            "*",
-            "js/vendor/*",
-            "!js/app/*"
-        ]
-    }))
+	envs: [
+		'browser'
+	],
+	globals: [
+		'$',
+		'jQuery'
+	],
+	ignore: [
+		'*',
+		'js/vendor/*',
+		'!js/app/*'
+	]
+}))
     .pipe(gulp.dest('./dist'))
 );
-
 
 gulp.task('default', () =>
     gulp.src('index.js')
     .pipe(xo({
-        "esnext": true
-    }))
+	esnext: true
+}))
     .pipe(gulp.dest('./dist'))
 );
